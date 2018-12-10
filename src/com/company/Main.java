2572ea2a6d85828ea0401c7fd2e17309e26d873f
ErnestoChe <ernestfilippov@mail.ru>
2018-12-10 17:25:27 +0300

    deleted main()

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,13 +1,6 @@
 package com.company;
 
-import java.util.ArrayList;
 
-public class Main {
-
-    public static void main(String[] args) {
-	// write your code here
-    }
-}
 class LinkedList
 {
     public Node head;
